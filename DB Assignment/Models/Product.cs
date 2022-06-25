794ec3ae1f04cd9afb00b0021b89ca56f10216ba
Mohammed AbuIbaid <m76eef@gmail.com>
2022-06-25 22:27:12 +0300

    Fix inherit (Product : BaseEntity)

--- DB Assignment/Models/Product.cs
+++ DB Assignment/Models/Product.cs
@@ -1,6 +1,6 @@
 ﻿namespace DB_Assignment.Models
 {
-    public class Product
+    public class Product : BaseEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
