577a3d1de8ba3a197dfae1b5ad316e0af1936883
Mohammed AbuIbaid <m76eef@gmail.com>
2022-06-25 22:24:37 +0300

    Add project files.

---
+++ DB Assignment/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using DB_Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DB_Assignment.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult MyName()
+        {
+            return Ok("My name is Mohammed Abu Ibaid");
+        }
+        [HttpPost]
+        public IActionResult Price()
+        {
+            Product product = new Product();
+            product.Name = "Acer Laptop";
+            product.Price = 1300.00;
+            return Ok($"{product.Name} Price is {product.Price}$");
+        }
+    }
+}
---
+++ DB Assignment/Models/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DB_Assignment.Models
+{
+    public class BaseEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDelete { get; set; }
+        public BaseEntity()
+        {
+            IsDelete = false;
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
